16ea0a53eff503ed86c7ff9789760ca3345530f8
CarlosFeria <hasckmc@gmail.com>
2015-10-07 19:32:55 +0200

    hello world2

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,7 @@
 public calss HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!!");
+		System.out.println("Hello world: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
 
 	}
 }
